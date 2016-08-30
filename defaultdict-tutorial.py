d008a779b43c942d9e0097a0f9a4005dc5be306f
Nathan Guenther <nathanpguenther@gmail.com>
2016-08-29 21:27:55 -0700

    First

---
+++ defaultdict-tutorial.py
@@ -0,0 +1,13 @@
+from collections import defaultdict
+d= defaultdict(list)
+
+# Inputs
+ints = raw_input().splice()
+a = []
+for i in range(ints[0])
+    a.append(i)
+b = []
+for i in range(ints[1])
+    b.append(i)
+
+# Logic
