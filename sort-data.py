f74cd779bd8669828962d3c308395fe7568385b0
Nathan Guenther <nathanpguenther@gmail.com>
2016-09-14 07:16:56 -0700

    Update sort-data.py
    Improve style, less code, more pythonic

--- sort-data.py
+++ sort-data.py
@@ -1,15 +1,8 @@
 # https://www.hackerrank.com/challenges/python-sort-sort
 N, M = map(int, raw_input().split())
-table = []
-for i in range(N):
-    table.append(map(int, raw_input().split()))
-
+table = [raw_input() for i in range(N)]
 K = int(raw_input())
 
-# Sort list by sublist[K]
-table.sort(key = lambda x: int(x[K]))
-
-for l in table:
-    for n in l:
-        print n,
-    print
+# Sort and print list by sublist[K]
+for row in sorted(table, key=lambda row: int(row.split()[K])):
+    print row
